523b649bab4d872163f8c712dec0ad443b326b3d
LeifTeorin <hjalle2000@gmail.com>
2023-04-03 11:34:45 +0200

    size works now

--- Labb 1/labh1.hs
+++ Labb 1/labh1.hs
@@ -16,11 +16,14 @@ sum' :: [Int] -> Int
 sum' [] = 0
 sum' (x : xs) = x + sum' xs
 
-sublists :: Int -> [Int] -> [(Int, [Int])]
+sum2 :: [[Int]] -> [(Int, [Int])]
+sum2 [] = [(0, [])]
+sum2 (x: xs) = [(sum' x, x)] ++ sum2 xs
+
+sublists :: Int -> [Int] -> [[Int]]
 sublists n xs = take (length xs - n + 1) (sublists' n xs)
-    where sublists' _ [] = [(0, [])]
-          sublists' 1 [x] = [(x, [x])]
-          sublists' n xs@(_:rest) = ((sum' xs), take n xs) : sublists' n rest
+    where sublists' _ [] = [[]]
+          sublists' n xs@(_:rest) = (take n xs) : sublists' n rest
 
 subsets :: [Int] -> [[Int]]
 subsets [] = [[]]
@@ -28,7 +31,7 @@ subsets (x:xs) = map (x:) (subsets xs) ++ (subsets xs)
 
 sumofsets :: [Int] -> [(Int, [Int])]
 sumofsets [] = []
-sumofsets xs = (nsubsets (length xs) xs)
+sumofsets xs = (sum2 (nsubsets (length xs) xs))
     where nsubsets 1 xs = sublists 1 xs
           nsubsets n xs = (sublists n xs) ++ nsubsets (n-1) xs
 
