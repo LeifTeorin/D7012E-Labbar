256bb9bda112b6422d7bf0620000bc7089a39ef9
Hjalmar <hjaolo-9@student.ltu.se>
2023-04-13 14:43:46 +0200

    labb1 är klar

--- Labb 1/labh1.hs
+++ Labb 1/labh1.hs
@@ -1,15 +1,17 @@
-import Distribution.Simple.Utils (xargs)
 list :: [Int]
 list = [-1, 2, -3, 4, -5]
+
+listbig :: [Int]
+listbig = map(\x -> x * (-1)^x) [1..99]
 
 list2 :: [[Int]]
 list2 = [[1,2,3], [4,5,6]]
 
-ins :: (Int, [Int]) -> [(Int, [Int])] -> [(Int, [Int])]
+ins :: (Int, Int, Int, [Int]) -> [(Int, Int, Int, [Int])] -> [(Int, Int, Int, [Int])]
 ins x [] = [x]
-ins (x, lst) ((y, lst2) : ys) = if x <= y then (x, lst) : (y, lst2) : ys else (y, lst2) : ins (x, lst) ys 
+ins (xsize, xi, xj, lst) ((ysize, yi, yj, lst2) : ys) = if xsize <= ysize then (xsize, xi, xj, lst) : (ysize, yi, yj, lst2) : ys else (ysize, yi, yj, lst2) : ins (xsize, xi, xj, lst) ys 
 
-iSort :: [(Int, [Int])] -> [(Int, [Int])]
+iSort :: [(Int, Int, Int, [Int])] -> [(Int, Int, Int, [Int])]
 iSort [] = []
 iSort (x : xs) = ins x (iSort xs)
 
@@ -17,37 +19,34 @@ sum' :: [Int] -> Int
 sum' [] = 0
 sum' (x : xs) = x + sum' xs
 
-sum2 :: [[Int]] -> [(Int, [Int])]
-sum2 [] = [(0, [])]
-sum2 (x: xs) = [(sum' x, x)] ++ sum2 xs
+sum2 :: [(Int, Int, [Int])] -> [(Int, Int, Int, [Int])]
+sum2 [] = [(0, -1, -1, [])]
+sum2 ((i, j, x): xs) = [(sum' x, i, j, x)] ++ sum2 xs
 
-sublists :: Int -> [Int] -> [[Int]]
-sublists n xs = take (length xs - n + 1) (sublists' n xs)
-    where sublists' _ [] = [[]]
-          sublists' n xs@(_:rest) = (take n xs) : sublists' n rest
+sublists :: Int -> [Int] -> [(Int, Int, [Int])]
+sublists n xs = take (length xs - n + 1) (sublists' 0 n xs)
+    where --sublists' i _ [x] = [(i, i, [x])]
+--          sublists' i 1 (xs:rest) = (i, i, (take 1 (xs:rest))) : sublists' (i+1) 1 rest
+          sublists' i n (xs:rest) = (i, (i + (n-1)), (take n (xs:rest))) : sublists' (i+1) n rest
 
-subsets :: [Int] -> [[Int]]
-subsets [] = [[]]
-subsets (x:xs) = map (x:) (subsets xs) ++ (subsets xs)
-
-sumofsets :: [Int] -> [(Int, [Int])]
+sumofsets :: [Int] -> [(Int, Int, Int, [Int])]
 sumofsets [] = []
 sumofsets xs = (sum2 (nsubsets (length xs) xs))
     where nsubsets 1 xs = sublists 1 xs
           nsubsets n xs = (sublists n xs) ++ nsubsets (n-1) xs
 
-ksmallest :: [Int] -> Int -> [(Int, [Int])]
+ksmallest :: [Int] -> Int -> [(Int, Int, Int, [Int])]
 ksmallest [] k = []
-ksmallest xs k = pickfromsortedK (iSort (sumofsets xs)) k
+ksmallest xs k = take k (iSort (sumofsets xs))
     where pickfromsortedK (x:xs) 1 = [x]
-          pickfromsortedK (x:xs) n = [x] ++ pickfromsortedK xs (n-1)
+          pickfromsortedK x n = take n x
 
 smallestKset :: [Int] -> Int -> IO ()
 smallestKset _ 0 = putStr("no sets to pick :,(")
-smallestKset xs k = putStr(smallestKstring (ksmallest xs k) k)
-    where smallestKstring ((size, lst):xs) k = "size: " ++ show(size) ++ "  " ++ "subset: " ++ show(lst) ++ "\n" ++ (smallestKstring xs (k-1))
+smallestKset xs k = putStr("Entire list: " ++ show(xs) ++ "     k = " ++ show(k) ++ "\n" ++ smallestKstring (ksmallest xs k) k)
+    where smallestKstring ((size, i, j, lst):xs) k = "size: " ++ show(size) ++ "  " ++ "i: " ++ show(i) ++ "    j: " ++ show(j) ++ "    subset: " ++ show(lst) ++ "\n" ++ (smallestKstring xs (k-1))
           smallestKstring _ 0 = "\n"
           smallestKstring [] _ = "\n"
 
 main :: IO ()
-main = smallestKset list 0
+main = smallestKset listbig 5
